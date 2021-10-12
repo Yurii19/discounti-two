70d46df373547ddff230dad6e1ca20ff2a12afd9
Yurii19 <yurikolechkin@gmail.com>
2021-10-12 21:46:13 +0300

    feature: added admin-container component

--- src/app/features/head/admin/admin.module.ts
+++ src/app/features/head/admin/admin.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin/admin.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AdminContainerComponent } from './admin-container/admin-container.component';
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, AdminContainerComponent],
   imports: [CommonModule, SharedModule],
-  exports: [AdminComponent, SharedModule],
+  exports: [AdminComponent, SharedModule, AdminContainerComponent],
 })
 export class AdminModule {}
