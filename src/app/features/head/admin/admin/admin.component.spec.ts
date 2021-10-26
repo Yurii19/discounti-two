f9acf407779c29ad3a610004d06be84e6dc9fffb
Yurii19 <yurikolechkin@gmail.com>
2021-10-26 11:51:30 +0300

    chore: tests for admin component added, instruction for start test added in readme

--- src/app/features/head/admin/admin/admin.component.spec.ts
+++ src/app/features/head/admin/admin/admin.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { EMPTY, of } from 'rxjs';
 
 import { AdminComponent } from './admin.component';
 
@@ -8,9 +10,9 @@ describe('AdminComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
-    })
-    .compileComponents();
+      declarations: [AdminComponent],
+      imports: [HttpClientModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +21,49 @@ describe('AdminComponent', () => {
     fixture.detectChanges();
   });
 
+  it('should call fetch method on click button', () => {
+    const spy = spyOn(component, 'receiveUser').and.callFake(() => {
+      return EMPTY;
+    });
+    component.addNewUser();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('userInfo length should be 3', () => {
+    const spy = spyOn(component, 'receiveUser').and.callFake(() => {
+      return of(['one: 1', 'two: 2', 'three: 3']);
+    });
+    component.addNewUser();
+
+    expect(component.userInfo.length).toBe(3);
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('initial value of usersNumber should be 0 ', () => {
+    component.setOfUsersNumber();
+    expect(component.usersNumber).toBe(0);
+  });
+
+  it(' newUserName shouldbe empty', () => {
+    component.clear();
+    expect(component.newUserName).toBe('');
+  });
+
+  it(' newUserName shouldbe equals to formcontrol', () => {
+    component.inputUser();
+    const formControlValue = component.inputUserFC.value;
+    expect(component.newUserName).toEqual(formControlValue);
+  });
+
+  it('text of button clear should be Clear', () => {
+    const btnClear = fixture.debugElement.nativeElement.querySelector('#clear');
+    expect(btnClear.innerHTML).toBe('Clear');
+  });
+
+  it('formcontrol inputUserFC shouldbe create ', () => {
+    expect(component.inputUserFC).toBeTruthy();
   });
 });
--- src/app/features/head/admin/admin/admin.component.ts
+++ src/app/features/head/admin/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-admin',
@@ -9,27 +10,22 @@ import { FormControl } from '@angular/forms';
 })
 export class AdminComponent implements OnInit {
   @Input() adminData: any = { name: 'Admin' };
+
   title: string = 'admin component';
   baseUrl: string = 'https://api.github.com/users';
-  newUserName: string;
+  newUserName: string = '';
   userInfo: any = [];
   userInfo1: any = [];
 
-  usersNumber =  0;
+  usersNumber = 0;
 
   userList: string[] = [];
 
-  inputUserFC = new FormControl('admin');
+  inputUserFC = new FormControl('');
 
-  constructor(private http: HttpClient) {
-    this.inputUserFC.patchValue('Yurko');
-    this.newUserName = this.inputUserFC.value;
-  }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-
-    
-  }
+  ngOnInit(): void {}
 
   clear() {
     this.inputUserFC.patchValue('');
@@ -38,18 +34,13 @@ export class AdminComponent implements OnInit {
 
   addNewUser() {
     const name = this.inputUserFC.value;
-    if(this.userList.includes(name)){
+    if (this.userList.includes(name)) {
       return;
     }
-    this.http.get(`${this.baseUrl}/${name}`).subscribe(
+    this.receiveUser(name).subscribe(
       (resp) => {
         this.userInfo = resp;
-        this.userList = [this.inputUserFC.value, ...this.userList]
-        console.log('Resp', resp)
-
-       // for (const key in resp) {
-        //  console.log(resp[key])
-        //  }
+        this.userList = [this.inputUserFC.value, ...this.userList];
       },
       (error) => {
         alert('The user does not exist !');
@@ -58,12 +49,15 @@ export class AdminComponent implements OnInit {
     );
   }
 
-  inputUser(val: any) {
-    const name = val.target.value;
-    this.newUserName = name;
+  receiveUser(login: string): Observable<any>{
+    return this.http.get(`${this.baseUrl}/${login}`);
   }
 
-  setOfUsersNumber(){
+  inputUser() {
+    this.newUserName = this.inputUserFC.value;
+  }
+
+  setOfUsersNumber() {
     this.usersNumber = this.userList.length;
   }
 }
